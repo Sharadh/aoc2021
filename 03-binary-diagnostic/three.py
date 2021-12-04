8df6b66472d8f482e8d99e556fd73df6b73912b1
Sharadh Krishnamurthy <sharadhk.murthy@gmail.com>
2021-12-03 17:14:46 -0800

    03: refactor to use tries

--- 03-binary-diagnostic/three.py
+++ 03-binary-diagnostic/three.py
@@ -1,33 +1,78 @@
 import click
-from collections import defaultdict, Counter
+
+
+class StringTrie:
+
+    def __init__(self, value="", parent=None):
+        self.value = value
+        self.parent = parent
+        self.children = {}
+        self.count = 0
+
+    def add(self, s):
+        self.count += 1
+
+        # Add subtrie as a child
+        c = s[0]
+        if c not in self.children:
+            self.children[c] = StringTrie(c, self)
+
+        # Termination case.
+        if len(s) == 1:
+            return
+
+        self.children[c].add(s[1:])
+
+    def add_many(self, *ss):
+        for s in ss:
+            self.add(s)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}<"{self.path}" x {self.count}>'
+
+    def __getitem__(self, item):
+        return self.children[item]
+
+    @property
+    def path(self):
+        result = ""
+        current = self
+        while current.parent:
+            result = current.value + result
+            current = current.parent
+        return result
 
 
 @click.command()
 @click.argument("input_file", type=click.File())
 def main(input_file):
-    lines = [l.strip() for l in input_file]
+    t = StringTrie()
+    for l in input_file:
+        t.add(l)
 
-    candidates = lines
-    i = 0
-    while len(candidates) > 1:
-        counts = [Counter(i) for i in zip(*candidates)]
-        need = '0' if counts[i]['0'] > counts[i]['1'] else '1'
-        candidates = [l for l in candidates if l[i] == need]
-        i += 1
-        # click.secho(f"{candidates}", fg="green")
+    current = t
+    while current.count > 1:
+        click.echo(current)
+        click.echo(current.children)
+        if current['0'].count > current['1'].count:
+            current = current['0']
+        else:
+            current = current['1']
         # click.prompt("")
-    ogen = int(candidates[0], 2)
+    ogen = int(str(current.path), 2)
+    click.echo(f"{current.path} --> {ogen}")
 
-    candidates = lines
-    i = 0
-    while len(candidates) > 1:
-        counts = [Counter(i) for i in zip(*candidates)]
-        need = '1' if counts[i]['0'] > counts[i]['1'] else '0'
-        candidates = [l for l in candidates if l[i] == need]
-        i += 1
-        # click.secho(f"{candidates}", fg="green")
+    current = t
+    while current.count > 1:
+        click.echo(current)
+        click.echo(current.children)
+        if current['0'].count > current['1'].count:
+            current = current['1']
+        else:
+            current = current['0']
         # click.prompt("")
-    co2 = int(candidates[0], 2)
+    co2 = int(str(current.path), 2)
+    click.echo(f"{current.path} --> {co2}")
 
     click.secho(f"ogen: {ogen}, co2: {co2}", fg="green")
     click.secho(f"{ogen * co2}", fg="green")
