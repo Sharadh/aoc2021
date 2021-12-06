e192d78c39632069f5d22576a5076e0183c297fc
Sharadh Krishnamurthy <sharadhk.murthy@gmail.com>
2021-12-05 23:23:53 -0800

    day06 - part one

---
+++ 06-lanternfish/main.py
@@ -0,0 +1,61 @@
+import click
+
+from collections import Counter
+
+
+@click.command()
+@click.argument("input_file", type=click.File())
+def main(input_file):
+    timers = [int(t) for t in input_file.readline().strip().split(",")]
+    counts = Counter(timers)
+    click.echo(str(counts))
+    click.prompt("?")
+
+    click.echo("Start simulation...")
+    # For timer = 1
+    # 1 : 1
+    # 2 : 0
+    # 3 : 6 8 (2)
+    # 4 : 5 7 (2)
+    # 5 : 4 6 (2)
+    # 6 : 3 5 (2)
+    # 7 : 2 4 (2)
+    # 8 : 1 3 (2)
+    # 9 : 0 2 (2)
+    # 10: 6 1 8 (3)
+    # 11: 5 0 7 (3)
+    # 12: 4 6 6 8 (4)
+    # 13: 3 5 5 7 (4)
+    # 14: 2 4 6 (3)
+    # 15: 1 3 5 (3)
+    # 16: 0 2 4 (3)
+    # 17: 6 1 3 8 (4)
+    # 18: 5 0 2 7 (4)
+    # 19: 4 6 1 6 8 (5)
+    # 20: 3 5 0 5 7 (5)
+    # 21: 2 4 6 4 6 8 (6)
+    result = {x: {} for x in range(1, 9)}
+    for day in range(81):
+        for timer in range(1, 9):
+            if timer >= day:
+                result[timer][day] = 1
+            else:
+                prev = max(day - timer - 1, 0)
+                result[timer][day] = result[6][prev] + result[8][prev]
+        click.echo(f"Day {day}: {str([result[t][day] for t in range(1, 9)])}")
+        # click.prompt("")
+
+    click.echo(str(counts))
+    click.prompt("?")
+
+    sum_ = 0
+    for timer, count in counts.items():
+        contribution = result[timer][80] * count
+        click.echo(f"f{timer} x {count} => {contribution}")
+        sum_ += contribution
+
+    click.secho(str(sum_), fg="green")
+
+
+if __name__ == "__main__":
+    main()
