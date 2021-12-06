ba08d7bbf8972e95bd9af4a01f6601ca9b5fe9ad
Sharadh Krishnamurthy <sharadhk.murthy@gmail.com>
2021-12-05 23:27:41 -0800

    day06 - part two

--- 06-lanternfish/main.py
+++ 06-lanternfish/main.py
@@ -5,11 +5,10 @@ from collections import Counter
 
 @click.command()
 @click.argument("input_file", type=click.File())
-def main(input_file):
+@click.argument("target_days", type=click.INT)
+def main(input_file, target_days):
     timers = [int(t) for t in input_file.readline().strip().split(",")]
     counts = Counter(timers)
-    click.echo(str(counts))
-    click.prompt("?")
 
     click.echo("Start simulation...")
     # For timer = 1
@@ -35,7 +34,7 @@ def main(input_file):
     # 20: 3 5 0 5 7 (5)
     # 21: 2 4 6 4 6 8 (6)
     result = {x: {} for x in range(1, 9)}
-    for day in range(81):
+    for day in range(target_days + 1):
         for timer in range(1, 9):
             if timer >= day:
                 result[timer][day] = 1
@@ -45,12 +44,12 @@ def main(input_file):
         click.echo(f"Day {day}: {str([result[t][day] for t in range(1, 9)])}")
         # click.prompt("")
 
-    click.echo(str(counts))
-    click.prompt("?")
+    click.echo(f"Input has following numbers of various timers: {counts}")
+    click.prompt("continue?")
 
     sum_ = 0
     for timer, count in counts.items():
-        contribution = result[timer][80] * count
+        contribution = result[timer][target_days] * count
         click.echo(f"f{timer} x {count} => {contribution}")
         sum_ += contribution
 
