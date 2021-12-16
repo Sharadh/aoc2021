00cac2bfb4f0d7647889a6d8e9563607b043ebce
Sharadh Krishnamurthy <sharadhk.murthy@gmail.com>
2021-12-15 16:49:06 -0800

    day15 - more diagnostic; gonna pivot

--- 15-chiton/main.py
+++ 15-chiton/main.py
@@ -8,6 +8,17 @@ import typing
 import heapq
 
 
+def print_grid(vertices, M, N):
+    lines = []
+    for i in range(M):
+        l = ""
+        for j in range(N):
+            d = vertices[i, j].distance
+            l += f" {d}".rjust(4, " ") if d < sys.maxsize else "   *"
+        lines.append(l)
+    click.echo("\n".join(lines))
+
+
 def neighbours(i, j, M, N):
     if i > 0:
         yield i-1, j
@@ -46,8 +57,8 @@ def vertex_swap(pq, i, j):
     pq[j].index = j
 
 
-def vertex_sift_down(pq, heaplen, i):
-    """Given heap pq of length l, sift down from index i to maintain heap property."""
+def vertex_sift_down(pq: typing.List[Vertex], heaplen, i):
+    """Given heap pq of length heaplen, sift down from index i to maintain heap property."""
     current = i
     l, r = left(current), right(current)
     while l < heaplen:
@@ -57,9 +68,11 @@ def vertex_sift_down(pq, heaplen, i):
                 child = r
             else:
                 child = l
-        elif pq[l].distance < pq[current].distance:
-            child = l
         else:
+            child = l
+
+        if pq[child].distance >= pq[current].distance:
+            # Invariant maintained; exit.
             break
 
         # Swap current and child nodes.
@@ -83,17 +96,13 @@ def vertex_sift_up(pq, i):
         current = p
 
 
-def vertex_insert(pq, heaplen, val):
-    val.index = heaplen
-    pq[heaplen], heaplen = val, heaplen + 1
-    vertex_sift_up(pq, heaplen - 1)
-
-
 def vertex_pop(pq, heaplen):
     result = pq[0]
 
     pq[0] = pq[heaplen - 1]
     pq[0].index = 0
+
+    pq[heaplen - 1] = None
     heaplen -= 1
 
     vertex_sift_down(pq, heaplen, 0)
@@ -103,23 +112,28 @@ def vertex_pop(pq, heaplen):
 
 @click.command()
 @click.argument("input_file", type=click.File())
-def main(input_file):
+@click.argument("expand", type=click.BOOL, default=False)
+def main(input_file, expand):
     risk = {}
     for i, l in enumerate(input_file):
         for j, val in enumerate(l.strip()):
             risk[(i, j)] = int(val)
 
-    M = N = (i + 1) * 5
-    DESTINATION = (M - 1, N - 1)
+    M = N = (i + 1)
 
-    # Expand the map...
-    for i in range(M):
-        for j in range(N):
-            risk_increase = (i // 10) + (j // 10)
-            original_risk = risk[i % 10, j % 10]
-            risk[i, j] = (original_risk + risk_increase)
-            if risk[i, j] > 9:
-                risk[i, j] -= 9
+    if expand:
+        M = N = (i + 1) * 5
+        # Expand the map...
+        for i in range(M):
+            for j in range(N):
+                risk_increase = (i // 10) + (j // 10)
+                original_risk = risk[i % 10, j % 10]
+                risk[i, j] = (original_risk + risk_increase)
+                if risk[i, j] > 9:
+                    risk[i, j] -= 9
+
+    DESTINATION = (M - 1, N - 1)
+    click.confirm(f"Grid size: {M}x{N}; destination is {DESTINATION}")
 
     vertices = {(i, j): Vertex(sys.maxsize, (i, j), 0) for i in range(M) for j in range(N)}
     vertices[0, 0].distance = 0
@@ -132,15 +146,31 @@ def main(input_file):
 
     heaplen = len(pq)
     unseen = {(i, j) for i in range(M) for j in range(N)}
+
+    click.confirm(f"Heap size: {heaplen}; Unseen size: {len(unseen)}")
     # click.echo(f"Starting with {len(unseen)} unseen vertices...")
 
+    i = 0
     while unseen:
+        # Diagnostics
+        print_grid(vertices, M, N)
+        click.echo(f"Visiting {pq[0].position} at distance={pq[0].distance}")
+        click.confirm("")
+
+        i += 1
         n, heaplen = vertex_pop(pq, heaplen)
         # click.echo(f"Removed element {n.position} (distance: {n.distance}) from the queue; heaplen={heaplen}")
         # click.echo(f"New min is: {pq[0]}")
 
         unseen.remove(n.position)
-        if n == DESTINATION:
+
+        if not i % 100:
+            # click.echo(f"Visiting {n.position} at distance={n.distance}")
+            # click.echo(str(pq[:5]))
+            # click.echo(str(pq[heaplen-5:heaplen]))
+            click.confirm(f"Round {i}")
+        if n.position == DESTINATION:
+            click.echo(f"Reached destination with {len(unseen)} unseen elements; exiting!")
             break
 
         for x, y in neighbours(*n.position, M, N):
@@ -151,11 +181,11 @@ def main(input_file):
             candidate = vertices[x, y]
             alt = n.distance + risk[x, y]
             if alt < candidate.distance:
-                # click.echo(f"{candidate.position} has a shorter path thro' {n.position}")
+                click.echo(f"{candidate.position} has a shorter path thro' {n.position}")
                 candidate.distance = alt
                 vertex_sift_up(pq, candidate.index)
 
-    click.secho(f"Answer found: {vertices[M-1, N-1].distance}! Exiting.", fg="green")
+    click.secho(f"Answer found: {vertices[DESTINATION].distance}! Exiting.", fg="green")
 
 
 if __name__ == "__main__":
